1026e0cda0613371be000cb16618abbf53234ec8
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-11-15 17:08:59 +0900

    feat: implements normal member
    ticket: TEST-4 (from jira)

--- src/banking.py
+++ src/banking.py
@@ -9,3 +9,8 @@ class OrdinaryMember(InterestRateStrategy):
     def apply_interest_rate(self, amount):
         interests = amount * 0.02
         return amount + interests
+
+class NormalMember(InterestRateStrategy):
+    def apply_interest_rate(self, amount):
+        interests = amount * 0.022
+        return amount + interests
