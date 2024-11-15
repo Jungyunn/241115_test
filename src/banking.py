f7c43def88a1c57d7b70505c945d244fc918cd25
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-11-15 17:02:19 +0900

    implements: Ordinary member

---
+++ src/banking.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class InterestRateStrategy(ABC):
+    @abstractmethod
+    def apply_interest_rate(self, amount):
+        pass
+    
+class OrdinaryMember(InterestRateStrategy):
+    def apply_interest_rate(self, amount):
+        interests = amount * 0.02
+        return amount + interests
