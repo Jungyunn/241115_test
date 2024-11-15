806980516e5e70e7945466ae2f3d5fb3e39ce544
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-11-15 17:14:43 +0900

    feat: implements premium
    issue: resolve #2
    ticket: TEST-5

--- src/banking.py
+++ src/banking.py
@@ -9,3 +9,8 @@ class OrdinaryMember(InterestRateStrategy):
     def apply_interest_rate(self, amount):
         interests = amount * 0.02
         return amount + interests
+
+class PremiumMember(InterestRateStrategy):
+    def apply_interest_rate(self, amount):
+        interests = amount * 0.024
+        return amount + interests
